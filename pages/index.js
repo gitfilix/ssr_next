6f3e0e2c9743a8c43f7f327924c3c166ffb68ffb
felix <runlevel07@gmail.com>
2019-11-03 12:59:36 +0100

    added axios and json data catch

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import MainLayout from '../components/layouts/mainLayout'
 
 class Home extends Component {
-  // getInitialProps is always expect an object
+  // getInitialProps is executing on server first and is always expect an object
   static getInitialProps() {
+    const  request = axios.get('http://jsonplaceholder.typicode.com/users/1')
+                      .then (response => {
+                        console.log('response', response.data);
+                      })
     console.log('getInitialProps');
 
-    return {}
+    return {
+      user: {
+        name: 'Francis',
+        lastname: 'Jones'
+      }
+    }
   }
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
-    console.log('constructor');
+    this.state = {
+      user: this.props.user
+    }
   }
 
   render() {
+    console.log('render: state user', this.state);
     return (
       <>
         <MainLayout>
