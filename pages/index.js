5aa7a7d0a3f84411ee867dfd861c48733a04e996
felix <runlevel07@gmail.com>
2019-11-03 11:03:38 +0100

    next js basic index page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <h1>Welcome to my new next.js page</h1>
+      </>
+    )
+  }
+}
+
+
+export default Home
