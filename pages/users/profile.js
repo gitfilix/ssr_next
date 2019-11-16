4dc43f023ec846077cba45f6ae6a41493af2e353
filiks <runlevel07@gmail.com>
2019-11-16 16:58:08 +0100

    user profile loaded with axios

--- pages/users/profile.js
+++ pages/users/profile.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
-import MainLayout from '../../components/layouts/mainLayout';
+import MainLayout from '../../components/layouts/mainLayout'
+import axios from 'axios'
+
 
 // classbased component
 class Profile extends Component {
   static async getInitialProps({query}) {
-    console.log('user query', query);
-    return {}
+    let user
+
+    try {
+      const response = await axios.get(`http://jsonplaceholder.typicode.com/users/${query.userId}`);
+        user = response.data
+    } catch(error) {
+      console.error(error)
+    }
+
+    return {user}
   }
 
+  showUser = (user) => (
+    <div>
+      <div>Name: {user.name}</div>
+      <div>lastname: </div>
+      <div>e-mail: {user.email}</div>
+    </div>
+  )
+
   render() {
+    console.log('user', this.props);
     return(
       <MainLayout>
-      something>>
+        {this.showUser(this.props.user)}
       </MainLayout>
     )
   }
